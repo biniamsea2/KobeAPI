90c344605b614fd31476a9e311c7b7622c1d657b
Biniam <Biniamt23@yahoo.com>
2020-09-01 21:40:25 -0700

    service folder created

---
+++ KobeBryant/KobeBryant/Models/Interface/IKobe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KobeBryant.Models.Interface
+{
+    interface IKobe
+    {
+        //will get a random record from the database, needs an id because we will use that to grab a random record
+        Task<Kobe> GetSinlgeRandomRecord(int id);
+
+        //will get a single record based on the ID given
+        Task<Kobe> GetSinlgeRecordById(int id);
+
+        //will get all the records from the database
+        Task<List<Kobe>> GetAllRecords();
+    }
+}
---
+++ KobeBryant/KobeBryant/Models/Service/KobeManager.cs
@@ -0,0 +1,26 @@
+﻿using KobeBryant.Models.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KobeBryant.Models.Service
+{
+    public class KobeManager : IKobe
+    {
+        public Task<List<Kobe>> GetAllRecords()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Kobe> GetSinlgeRandomRecord(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Kobe> GetSinlgeRecordById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
