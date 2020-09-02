27bb9469bc9a0a08889111b85a112ff3add4996a
Biniam <Biniamt23@yahoo.com>
2020-09-01 21:22:22 -0700

    model created

---
+++ KobeBryant/KobeBryant/Models/Kobe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KobeBryant.Models
+{
+    public class Kobe
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Team { get; set; }
+        public int Age { get; set; }
+        public int Year { get; set; }
+        public DateTime Date { get; set; }
+        public string Record { get; set; }
+    }
+}
