0dd89406efcfea4d0c4ea3cf8d81eac8b824e5e3
Biniam <Biniamt23@yahoo.com>
2020-09-01 23:03:13 -0700

    database created

---
+++ KobeBryant/KobeBryant/Data/KobeBryantDbContext.cs
@@ -0,0 +1,22 @@
+﻿using KobeBryant.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KobeBryant.Data
+{
+    public class KobeBryantDbContext : DbContext
+    {
+        public KobeBryantDbContext(DbContextOptions<KobeBryantDbContext> options) : base(options)
+        {
+
+        }
+
+
+
+        public DbSet<Kobe> Records { get; set; }
+
+    }
+}
